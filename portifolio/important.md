9ccc1d471907b8453a742d97a8a40585797d57e6
gabriCsJs <gabrifreisilva@outlook.com>
2024-04-20 15:32:08 -0300

    first commit

---
+++ portifolio/important.md
@@ -0,0 +1,7 @@
+Lincks relacionados ao projeto
+
+https://www.youtube.com/watch?v=bP83UT3Mpuk&t=63s video fonte do nosso site
+
+pegar codigos prontos
+https://www.brasilcode.com.br/35-botoes-css-com-animacao/ botoes legais
+https://codepen.io/
---
+++ portifolio/script.js
@@ -0,0 +1,10 @@
+// function changeBackgroundColor() { /* MUDA A COR DO SITE*/
+//     const body = document.body;
+//     const currentBackgroundColor = body.style.backgroundColor;
+
+//     if (currentBackgroundColor === "rgb(13, 13, 13)") {
+//         body.style.backgroundColor = "#F2F2F2";
+//     } else {
+//         body.style.backgroundColor = "#0D0D0D";
+//     }
+// }
